7319cc6d5a9d57197ffa4338b291cacd8faa1335
Telegram & App source Code <khithlainhtet2021@gmail.com>
2026-03-04 23:12:19 +0630

    MYANMARMUSIC

--- MYANMARMUSIC/utils/inline/play.py
+++ MYANMARMUSIC/utils/inline/play.py
@@ -89,8 +89,8 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="💬 sᴜᴘᴘᴏʀᴛ", url=SUPPORT_GROUP),
-            InlineKeyboardButton(text="📢 ᴄʜᴀɴɴᴇʟ", url=SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="🌲sᴜᴘᴘᴏʀᴛ🌲", url=SUPPORT_GROUP),
+            InlineKeyboardButton(text="🌻ᴄʜᴀɴɴᴇʟ🌻", url=SUPPORT_CHANNEL),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
